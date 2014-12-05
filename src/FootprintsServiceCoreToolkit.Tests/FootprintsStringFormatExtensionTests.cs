a3acbc4b4290ca40ee0c3c9a8a57d949b3afd30a
Geoffrey Huntley <ghuntley@ghuntley.com>
2014-12-05 14:23:47 +1100

    initial commit

---
+++ src/FootprintsServiceCoreToolkit.Tests/FootprintsStringFormatExtensionTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FootprintsServiceCoreToolkit.Tests
+{
+    [TestFixture]
+    public class FootprintsStringFormatExtensionTests
+    {
+        [Test]
+        public void FromFootprintsStringShouldDecodeToExpected()
+        {
+            const string @null = null;
+            Assert.IsNull(@null.FromFootprints());
+            Assert.AreSame(String.Empty, String.Empty.FromFootprints());
+
+            Assert.AreEqual("_HISTORY_LINE_BREAK_".FromFootprints(), "\\n");
+
+            Assert.AreEqual("__b".FromFootprints(), " ");
+
+            Assert.AreEqual("__a".FromFootprints(), "'");
+            Assert.AreEqual("__q".FromFootprints(), "\"");
+            Assert.AreEqual("__t".FromFootprints(), "`");
+            Assert.AreEqual("__m".FromFootprints(), "@");
+            Assert.AreEqual("__d".FromFootprints(), ".");
+            Assert.AreEqual("__u".FromFootprints(), "-");
+
+            Assert.AreEqual("__s".FromFootprints(), ";");
+            Assert.AreEqual("__c".FromFootprints(), ":");
+            Assert.AreEqual("__p".FromFootprints(), ")");
+            Assert.AreEqual("__P".FromFootprints(), "(");
+            Assert.AreEqual("__3".FromFootprints(), "#");
+            Assert.AreEqual("__4".FromFootprints(), "$");
+            Assert.AreEqual("__5".FromFootprints(), "%");
+            Assert.AreEqual("__6".FromFootprints(), "^");
+            Assert.AreEqual("__7".FromFootprints(), "&");
+            Assert.AreEqual("__8".FromFootprints(), "*");
+
+            Assert.AreEqual("__0".FromFootprints(), "~");
+            Assert.AreEqual("__f".FromFootprints(), "/");
+            Assert.AreEqual("__F".FromFootprints(), "\\");
+            Assert.AreEqual("__Q".FromFootprints(), "?");
+            Assert.AreEqual("__e".FromFootprints(), "]");
+            Assert.AreEqual("__E".FromFootprints(), "[");
+
+            Assert.AreEqual("__g".FromFootprints(), ">");
+            Assert.AreEqual("__G".FromFootprints(), "<");
+            Assert.AreEqual("__B".FromFootprints(), "!");
+            Assert.AreEqual("__W".FromFootprints(), "{");
+            Assert.AreEqual("__w".FromFootprints(), "}");
+            Assert.AreEqual("__C".FromFootprints(), "=");
+            Assert.AreEqual("__A".FromFootprints(), "+");
+            Assert.AreEqual("__I".FromFootprints(), "|");
+            Assert.AreEqual("__M".FromFootprints(), ",");
+        }
+
+        [Test]
+        public void ToFootprintsStringShouldEncodeToExpected()
+        {
+            const string @null = null;
+            Assert.IsNull(@null.ToFootprints());
+            Assert.AreEqual(String.Empty, String.Empty.ToFootprints());
+
+            Assert.AreEqual(" ".ToFootprints(), "__b");
+
+            Assert.AreEqual("'".ToFootprints(), "__a");
+            Assert.AreEqual("\"".ToFootprints(), "__q");
+            Assert.AreEqual("`".ToFootprints(), "__t");
+            Assert.AreEqual("@".ToFootprints(), "__m");
+            Assert.AreEqual(".".ToFootprints(), "__d");
+            Assert.AreEqual("-".ToFootprints(), "__u");
+
+            Assert.AreEqual(";".ToFootprints(), "__s");
+            Assert.AreEqual(":".ToFootprints(), "__c");
+            Assert.AreEqual(")".ToFootprints(), "__p");
+            Assert.AreEqual("(".ToFootprints(), "__P");
+            Assert.AreEqual("#".ToFootprints(), "__3");
+            Assert.AreEqual("$".ToFootprints(), "__4");
+            Assert.AreEqual("%".ToFootprints(), "__5");
+            Assert.AreEqual("^".ToFootprints(), "__6");
+            Assert.AreEqual("&".ToFootprints(), "__7");
+            Assert.AreEqual("*".ToFootprints(), "__8");
+
+            Assert.AreEqual("~".ToFootprints(), "__0");
+            Assert.AreEqual("/".ToFootprints(), "__f");
+            Assert.AreEqual("\\".ToFootprints(), "__F");
+            Assert.AreEqual("?".ToFootprints(), "__Q");
+            Assert.AreEqual("]".ToFootprints(), "__e");
+            Assert.AreEqual("[".ToFootprints(), "__E");
+
+            Assert.AreEqual(">".ToFootprints(), "__g");
+            Assert.AreEqual("<".ToFootprints(), "__G");
+            Assert.AreEqual("!".ToFootprints(), "__B");
+            Assert.AreEqual("{".ToFootprints(), "__W");
+            Assert.AreEqual("}".ToFootprints(), "__w");
+            Assert.AreEqual("=".ToFootprints(), "__C");
+            Assert.AreEqual("+".ToFootprints(), "__A");
+            Assert.AreEqual("|".ToFootprints(), "__I");
+            Assert.AreEqual(",".ToFootprints(), "__M");
+        }
+    }
+}
---
+++ src/FootprintsServiceCoreToolkit/FootprintsStringFormatExtension.cs
@@ -0,0 +1,100 @@
+﻿namespace FootprintsServiceCoreToolkit
+{
+    public static class FootprintsStringFormatExtension
+    {
+        /// <remarks>
+        ///     Based off FixFieldsInteraction.js which comes as part of Footprints ServiceCore.
+        /// </remarks>
+        public static string FromFootprints(this string data)
+        {
+            if (string.IsNullOrEmpty(data)) return data;
+
+            data = data.Replace("_HISTORY_LINE_BREAK_", "\\n");
+
+            data = data.Replace("__b", " ");
+
+            data = data.Replace("__a", "'");
+            data = data.Replace("__q", "\"");
+            data = data.Replace("__t", "`");
+            data = data.Replace("__m", "@");
+            data = data.Replace("__d", ".");
+            data = data.Replace("__u", "-");
+
+            data = data.Replace("__s", ";");
+            data = data.Replace("__c", ":");
+            data = data.Replace("__p", ")");
+            data = data.Replace("__P", "(");
+            data = data.Replace("__3", "#");
+            data = data.Replace("__4", "$");
+            data = data.Replace("__5", "%");
+            data = data.Replace("__6", "^");
+            data = data.Replace("__7", "&");
+            data = data.Replace("__8", "*");
+
+            data = data.Replace("__0", "~");
+            data = data.Replace("__f", "/");
+            data = data.Replace("__F", "\\");
+            data = data.Replace("__Q", "?");
+            data = data.Replace("__e", "]");
+            data = data.Replace("__E", "[");
+
+            data = data.Replace("__g", ">");
+            data = data.Replace("__G", "<");
+            data = data.Replace("__B", "!");
+            data = data.Replace("__W", "{");
+            data = data.Replace("__w", "}");
+            data = data.Replace("__C", "=");
+            data = data.Replace("__A", "+");
+            data = data.Replace("__I", "|");
+            data = data.Replace("__M", ",");
+
+            return data;
+        }
+
+        /// <remarks>
+        ///     Based off FixFieldsInteraction.js which comes as part of Footprints ServiceCore.
+        /// </remarks>
+        public static string ToFootprints(this string data)
+        {
+            if (string.IsNullOrEmpty(data)) return data;
+            data = data.Replace(" ", "__b");
+
+            data = data.Replace("'", "__a");
+            data = data.Replace("\"", "__q");
+            data = data.Replace("`", "__t");
+            data = data.Replace("@", "__m");
+            data = data.Replace(".", "__d");
+            data = data.Replace("-", "__u");
+
+            data = data.Replace(";", "__s");
+            data = data.Replace(":", "__c");
+            data = data.Replace(")", "__p");
+            data = data.Replace("(", "__P");
+            data = data.Replace("#", "__3");
+            data = data.Replace("$", "__4");
+            data = data.Replace("%", "__5");
+            data = data.Replace("^", "__6");
+            data = data.Replace("&", "__7");
+            data = data.Replace("*", "__8");
+
+            data = data.Replace("~", "__0");
+            data = data.Replace("/", "__f");
+            data = data.Replace("\\", "__F");
+            data = data.Replace("?", "__Q");
+            data = data.Replace("]", "__e");
+            data = data.Replace("[", "__E");
+
+            data = data.Replace(">", "__g");
+            data = data.Replace("<", "__G");
+            data = data.Replace("!", "__B");
+            data = data.Replace("{", "__W");
+            data = data.Replace("}", "__w");
+            data = data.Replace("=", "__C");
+            data = data.Replace("+", "__A");
+            data = data.Replace("|", "__I");
+            data = data.Replace(",", "__M");
+
+            return data;
+        }
+    }
+}
